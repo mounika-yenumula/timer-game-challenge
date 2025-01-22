62ff834c93e3928aae967cd5c563c57fda76bc65
mounika-yenumula <mounikamadhav24@gmail.com>
2025-01-21 22:32:35 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1}/>
+        <TimerChallenge title="Tough" targetTime={5}/>
+        <TimerChallenge title="Extra Tough" targetTime={10}/>
+        <TimerChallenge title="Hard" targetTime={15}/>
+        </div>
+
     </>
   );
 }
